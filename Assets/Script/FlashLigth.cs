6421e17a10816a653a3e363a89913ed98d1dc964
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-03-08 01:29:57 -0400

    Update

---
+++ Assets/Script/FlashLigth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLigth : MonoBehaviour
+{
+
+    public Light Linterna;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Linterna"))
+        {
+            if(Linterna.enabled)
+            {
+                Linterna.enabled = false;
+            }
+            else if(Linterna.enabled == false)
+            {
+                Linterna.enabled = true;
+            }
+        }
+    }
+}
